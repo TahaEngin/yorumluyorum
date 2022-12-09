9752908f0a8b162ad1094d759a64107e9dae11cf
BARDAKKIRAN <tahaengin02@gmail.com>
2022-12-09 19:59:49 +0300

    siu

--- lib/Templates/CommentCard.dart
+++ lib/Templates/CommentCard.dart
@@ -11,33 +11,42 @@ class CommentCard extends StatefulWidget {
 class _CommentCardState extends State<CommentCard> {
   @override
   Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
     return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child:  Column(
-        children:[
-          Padding(
             padding: const EdgeInsets.all(4.0),
             child: Container(
+              color: Colors.pink,
+              height: w/4,
               child: Row(
                 children: [
-                  Image.asset(widget.image,height: 50,width: 50,),
+                  Container(
+                    width: w/4,
+                    height: w/4,
+                    child: Image.network(widget.image,height: w/8,width:w/8)),
                   Column(
                     children: [
                       Row(
                         children: [
-                          Text(widget.nickname,style: TextStyle(fontSize: 22.0),),
-                          Text(widget.username,style: TextStyle(fontSize: 16.0),),
+                          Text(widget.nickname,style: TextStyle(fontSize: w/16),),
+                          Text(widget.username,style: TextStyle(fontSize: w/17),),
                         ],
                       ),
-                      Text(widget.comment,style: TextStyle(fontSize: 18.0),),
+                      Text(widget.comment,style: TextStyle(fontSize: w/16.5),),
+                      Row(
+                        children: [
+                          SizedBox(width: 5*w/16),
+                          Icon(Icons.comment,size:w/32),
+                          SizedBox(width: 3*w/8),
+                          Icon(Icons.favorite,size: w/32),
+                          SizedBox(width: 5*w/16),
+                        ],
+                      ),
                     ],
                   ),
                 ],
               ),
             ),
-          ),
-        ] 
-      ),
-    );
+          );
   }
 }
